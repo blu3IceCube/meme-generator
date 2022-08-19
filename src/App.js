3cf9ea862d07dbe903f57a14813862b551879659
blu3IceCube <111587317+blu3IceCube@users.noreply.github.com>
2022-08-19 21:34:12 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import MemeCard from "./components/memeCard";
+import "../src/styles.css";
+
+export default function App() {
+    return (
+        <MemeCard/>
+    )
+}
