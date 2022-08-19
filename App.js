0e627a12152e8af20f245e41cf0af1f417a8372d
blu3IceCube <111587317+blu3IceCube@users.noreply.github.com>
2022-08-19 21:38:00 +0530

    Delete App.js

--- App.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import MemeCard from "./components/memeCard";
-import "../src/styles.css";
-
-export default function App() {
-    return (
-        <MemeCard/>
-    )
-}
